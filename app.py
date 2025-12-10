0c8198ed9ab522fd9c579a2ddd8f4510e37cfcb2
ryanb519 <53130731+ryanb519@users.noreply.github.com>
2025-12-10 11:43:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ st.set_page_config(page_title="NFBC Standings Predictor", layout="wide")
 # -----------------------
 # UI
 # -----------------------
-col_logo, col_title = st.columns([1, 4])
+col_logo, col_title = st.columns([1, 2])
 
 with col_logo:
     st.image("_Wordmark.png", width=80)   # adjust size as needed
