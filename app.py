b6bdc3baf0d504f404a0d5864c2e40399d62b72d
ryanb519 <53130731+ryanb519@users.noreply.github.com>
2025-12-10 11:16:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -406,12 +406,19 @@ with right_col:
             standings_df = standings_df.drop(columns=['R_Rank','RBI_Rank','HR_Rank','SB_Rank','AVG_Rank','ERA_Rank','WHIP_Rank','SO_Rank','W_Rank','SV_Rank'])
             standings_df = standings_df.rename(columns={'Overall_Rank':'Rank','DraftTeam':'Team','Grand_Total_Score':'Total Points','Hitter_Score':'Hitters','Pitcher_Score':'Pitchers'})
 
-            # Apply red-green color formatting to Standings
             styler = standings_df.style
-            color_cols = ["R", "HR", "RBI", "SB", "W", "SO", "SV", "AVG"]
-            styler = standings_df.style  # create a Styler once
-            styler = styler.apply(lambda s: color_metric_diverging(s, higher_is_better=True), subset=["ERA", "WHIP"])
-            styler = styler.apply(lambda s: color_metric_diverging(s, higher_is_better=False), subset=color_cols)
-                                      
-            #results_placeholder.dataframe(standings_df, use_container_width=True, hide_index=True, height=575)
-            results_placeholder.dataframe(styler, use_container_width=True, hide_index=True, height=575)
+            higher_is_better_cols = ["R", "HR", "RBI", "SB", "W", "SO", "SV", "AVG"]
+            lower_is_better_cols = ["ERA", "WHIP"]
+    
+            styler = styler.apply(
+                lambda s: color_metric_diverging(s, True),
+                subset=[c for c in higher_is_better_cols if c in standings_df.columns],
+            )
+            styler = styler.apply(
+                lambda s: color_metric_diverging(s, False),
+                subset=[c for c in lower_is_better_cols if c in standings_df.columns],
+            )
+    
+            standings_placeholder.dataframe(
+                styler, use_container_width=True, hide_index=True, height=575
+            )
