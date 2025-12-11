b6a64d5055f34829b3da90b13e8128d93dcc310a
ryanb519 <53130731+ryanb519@users.noreply.github.com>
2025-12-11 08:59:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -403,5 +403,5 @@ with right_col:
             )
     
             standings_placeholder.dataframe(
-                styler, use_container_width=True, hide_index=True, height=575
+                styler, width='stretch', hide_index=True, height=575
             )
