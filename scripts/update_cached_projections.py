c947efa70f81768ac61867e79da8bb1045bf1a6d
ryanb519 <53130731+ryanb519@users.noreply.github.com>
2026-04-09 08:10:40 -0700

    Update update_cached_projections.py

--- scripts/update_cached_projections.py
+++ scripts/update_cached_projections.py
@@ -51,26 +51,26 @@ for i in projList:
 #SCHEDULE
 data = requests.get("https://www.fangraphs.com/api/roster-resource/schedule-grid/data").json()
 df = pd.DataFrame(data)
-upload_df_to_github(df, repo_name, "data/fangraphs/schedule.json", "Daily update: hitters", github_token)
+upload_df_to_github(df, repo_name, "data/fangraphs/schedule.csv", "Daily update: hitters", github_token)
 
 #PROBABLES
 data = requests.get("https://www.fangraphs.com/api/roster-resource/probables-grid/data").json()
 df = pd.DataFrame(data)
-upload_df_to_github(df, repo_name, "data/fangraphs/probables.json", "Daily update: hitters", github_token)
+upload_df_to_github(df, repo_name, "data/fangraphs/probables.csv", "Daily update: hitters", github_token)
 
 #2026 HITTER STATS
 data = requests.get("https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=bat&lg=all&qual=5&season=2026&season1=2026&startdate=2026-03-01&enddate=2026-11-01&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=8&postseason=&sortdir=default&sortstat=WAR").json()
 data = data['data']
 data = pd.DataFrame(data)
 data['IDfg'] = data['playerid']
-upload_df_to_github(df, repo_name, "data/fangraphs/fgb26.json", "Daily update: hitters", github_token)
+upload_df_to_github(df, repo_name, "data/fangraphs/fgb26.csv", "Daily update: hitters", github_token)
 
 #2026 PITCHER STATS
 data = requests.get("https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=pit&lg=all&qual=5&season=2026&season1=2026&startdate=2026-03-01&enddate=2026-11-01&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=8&postseason=&sortdir=default&sortstat=WAR").json()
 data = data['data']
 data = pd.DataFrame(data)
 data['IDfg'] = data['playerid']
-upload_df_to_github(df, repo_name, "data/fangraphs/fgp26.json", "Daily update: pitchers", github_token)
+upload_df_to_github(df, repo_name, "data/fangraphs/fgp26.csv", "Daily update: pitchers", github_token)
 
 #TEAM LEVEL
 ytdData = requests.get('https://www.fangraphs.com/api/leaders/major-league/data?pos=all&stats=bat&lg=all&qual=y&season=2026&season1=2026&ind=0&team=0%2Cts&type=8&month=0').json()
@@ -89,7 +89,7 @@ vL = pd.DataFrame(vLdata)
 vR = pd.DataFrame(vRdata)
 l30 = pd.DataFrame(l30Data)
 
-upload_df_to_github(ytd, repo_name, "data/fangraphs/month0.json", "Daily update: pitchers", github_token)
-upload_df_to_github(vL, repo_name, "data/fangraphs/month13.json", "Daily update: pitchers", github_token)
-upload_df_to_github(vR, repo_name, "data/fangraphs/month13.json", "Daily update: pitchers", github_token)
-upload_df_to_github(l30, repo_name, "data/fangraphs/month3.json", "Daily update: pitchers", github_token)
+upload_df_to_github(ytd, repo_name, "data/fangraphs/month0.csv", "Daily update: pitchers", github_token)
+upload_df_to_github(vL, repo_name, "data/fangraphs/month13.csv", "Daily update: pitchers", github_token)
+upload_df_to_github(vR, repo_name, "data/fangraphs/month13.csv", "Daily update: pitchers", github_token)
+upload_df_to_github(l30, repo_name, "data/fangraphs/month3.csv", "Daily update: pitchers", github_token)
